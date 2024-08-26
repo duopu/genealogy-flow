54d36c0d50faae681ba7fe588edf7f2242fb783d
jiangeq <jiangeq@digiwin.com>
2024-08-26 10:15:42 +0800

    feat: add server support

--- vite.config.ts
+++ vite.config.ts
@@ -9,6 +9,7 @@ import Components from 'unplugin-vue-components/vite'
 
 // https://vitejs.dev/config/
 export default defineConfig({
+  base: '/genealogy-flow/',
   plugins: [
     vue(),
     vueJsx(),
